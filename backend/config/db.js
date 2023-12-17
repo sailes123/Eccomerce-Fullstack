8648f17d91fe6264bbf6e4530ebe26ddc30822a1
Sailes123 <saileshbajgain056@gmail.com>
2023-12-17 17:58:02 +0545

    database connected

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDataBase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`Mongodb is connected: ${data.connection.host}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export { connectDataBase };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
+import { connectDataBase } from "./config/db.js";
 
 dotenv.config({
         path: 'backend/config/.env'
 })
+
+// Connect Database
+
+connectDataBase();
 
 app.listen(process.env.PORT, ()=> {
         console.log(`Server is working on https://localhost:${process.env.PORT}`)
